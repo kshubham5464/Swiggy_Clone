b0cf360c986071e262a073688e8282759f5e2d89
kshubham5464 <kshubham5462@gmail.com>
2025-04-02 22:40:18 +0530

    Initial commit

---
+++ src/Common/Header.js
@@ -0,0 +1,17 @@
+const Header = () => {
+    return (
+        <div className ="header">
+            <img alt ="Logo" src={"https://th.bing.com/th/id/OIP._ViV12AGgkktgO0bh02AGwHaK9?w=115&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7"}/>
+            <ul>
+                <li>Home</li>
+                <li>Help</li>
+                <li>Search</li>
+                <li>Cart</li>
+            </ul>
+        </div>
+    )
+}
+export default Header
+
+
+
